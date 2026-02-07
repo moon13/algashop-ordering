5f0fdaa33acf0be6edc804ae9edf8fa0f69c9727
Fernanda <fernanda.g.araujo@gmail.com>
2026-02-06 21:00:45 -0300

    commit de tasks de testes de integração.

---
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/entity/OrderPersistenceEntityIT.java
@@ -0,0 +1,15 @@
+package com.algaworks.algashop.ordering.infrastructure.persistence.entity;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPersistenceEntityIT {
+
+    @Test
+     public void shouldRun(){
+
+     }
+
+}
