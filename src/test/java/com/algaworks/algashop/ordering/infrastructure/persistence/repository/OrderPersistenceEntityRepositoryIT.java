25a4399838fe96acff6e87f32328525219b6b1e0
Fernanda <fernanda.g.araujo@gmail.com>
2026-02-23 20:59:50 -0300

    commit aula de auditoria

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -1,6 +1,7 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
 
 
+import com.algaworks.algashop.ordering.infrastructure.persistence.config.SpringDataAuditingConfig;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntityTestDataBuilder;
 import org.assertj.core.api.Assertions;
@@ -8,10 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace =  AutoConfigureTestDatabase.Replace.NONE)
+@Import(SpringDataAuditingConfig.class)
 class OrderPersistenceEntityRepositoryIT {
 
     private final OrderPersistenceEntityRepository orderPersistenceEntityRepository;
@@ -34,5 +37,16 @@ class OrderPersistenceEntityRepositoryIT {
          long ordersCount = orderPersistenceEntityRepository.count();
          Assertions.assertThat(ordersCount).isZero();
     }
+
+    @Test
+     public void shouldSetAuditingValues(){
+        OrderPersistenceEntity entity = OrderPersistenceEntityTestDataBuilder.existingOrder().build();
+        entity = orderPersistenceEntityRepository.saveAndFlush(entity);
+
+          Assertions.assertThat(entity.getCreatedByUserId()).isNotNull();
+          Assertions.assertThat(entity.getLastModifiedAt()).isNotNull();
+          Assertions.assertThat(entity.getLastModifiedByUserId()).isNotNull();
+
+     }
 
 }
