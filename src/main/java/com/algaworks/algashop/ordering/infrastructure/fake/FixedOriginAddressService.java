1d4205b4fa44051371b8eeb521b6779cb513eb87
Fernanda <fernanda.g.araujo@gmail.com>
2026-05-12 21:13:38 -0300

    commit de integracao rapidex-api conserto

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/fake/FixedOriginAddressService.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/fake/FixedOriginAddressService.java
@@ -3,7 +3,9 @@ package com.algaworks.algashop.ordering.infrastructure.fake;
 import com.algaworks.algashop.ordering.domain.model.service.OriginAddressService;
 import com.algaworks.algashop.ordering.domain.model.valueobject.Address;
 import com.algaworks.algashop.ordering.domain.model.valueobject.ZipCode;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixedOriginAddressService implements OriginAddressService {
 
     @Override
