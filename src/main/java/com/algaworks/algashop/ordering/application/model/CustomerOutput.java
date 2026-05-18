f04a245de00fa0c78bef89cf2294df6c2a865f6b
Fernanda <fernanda.g.araujo@gmail.com>
2026-05-17 21:18:51 -0300

    commit de customer application service.

---
+++ src/main/java/com/algaworks/algashop/ordering/application/model/CustomerOutput.java
@@ -0,0 +1,39 @@
+package com.algaworks.algashop.ordering.application.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomerOutput {
+
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String document;
+    private String phone;
+    private LocalDate birthDate;
+    private Integer loyaltyPoints;
+    private OffsetDateTime registeredAt;
+    private OffsetDateTime archivedAt;
+    private Boolean promotionNotificationsAllowed;
+    private Boolean archived;
+    private AddressData address;
+
+}
--- src/main/java/com/algaworks/algashop/ordering/application/service/CustomerManagementApplicationService.java
+++ src/main/java/com/algaworks/algashop/ordering/application/service/CustomerManagementApplicationService.java
@@ -3,6 +3,7 @@ package com.algaworks.algashop.ordering.application.service;
 
 import com.algaworks.algashop.ordering.application.model.AddressData;
 import com.algaworks.algashop.ordering.application.model.CustomerInput;
+import com.algaworks.algashop.ordering.application.model.CustomerOutput;
 import com.algaworks.algashop.ordering.domain.model.commons.*;
 import com.algaworks.algashop.ordering.domain.model.customer.*;
 import lombok.RequiredArgsConstructor;
@@ -45,6 +46,37 @@ public class CustomerManagementApplicationService {
         customers.add(customer);
 
         return customer.id().value();
+    }
+
+    @Transactional(readOnly = true)
+     public CustomerOutput findById(UUID customerId) {
+          Objects.requireNonNull(customerId);
+        Customer customer = customers.ofId(new CustomerId(customerId))
+                .orElseThrow(() -> new CustomerNotFoundException());
+
+        return CustomerOutput.builder()
+                .id(customer.id().value())
+                .firstName(customer.fullName().firstName())
+                .lastName(customer.fullName().lastName())
+                .email(customer.email().value())
+                .document(customer.document().value())
+                .phone(customer.phone().value())
+                .promotionNotificationsAllowed(customer.isPromotionNotificationsAllowed())
+                .loyaltyPoints(customer.loyaltyPoints().value())
+                .registeredAt(customer.registeredAt())
+                .archived(customer.isArchived())
+                .archivedAt(customer.archivedAt() != null ? customer.archivedAt() : null)
+                .birthDate(customer.birthDate() != null ? customer.birthDate().value() : null)
+                .address(AddressData.builder()
+                        .street(customer.address().street())
+                        .number(customer.address().number())
+                        .complement(customer.address().complement())
+                        .neighborhood(customer.address().neightborhood())
+                        .city(customer.address().city())
+                        .state(customer.address().state())
+                        .zipCode(customer.address().zipCode().value())
+                        .build())
+                .build();
     }
 
 }
