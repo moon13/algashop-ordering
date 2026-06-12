3e939bcb218930e2fe0b291397ebdf361f358122
Fernanda <fernanda.g.araujo@gmail.com>
2026-06-12 20:43:46 -0300

    commit de event listener de customer.

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/listener/customer/CustomerEventListener.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/listener/customer/CustomerEventListener.java
@@ -10,23 +10,19 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class CustomerEventListener {
 
-
     @EventListener
-    public void listener(CustomerRegisteredEvent customerRegisteredEvent) {
+    public void listen(CustomerRegisteredEvent event) {
         log.info("CustomerRegisteredEvent listen 1");
-
     }
 
     @EventListener
-    public void listenerSecondary(CustomerRegisteredEvent customerRegisteredEvent) {
+    public void listenSecondary(CustomerRegisteredEvent event) {
         log.info("CustomerRegisteredEvent listen 2");
-
     }
 
     @EventListener
-    public void listenerArchived(CustomerArchivedEvent customerArchivedEvent) {
+    public void listen(CustomerArchivedEvent event) {
         log.info("CustomerArchivedEvent listen 1");
-
     }
 
 }
