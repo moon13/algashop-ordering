2242e3d3f590a0bb4aad05502c5c3fa524644e6d
Fernanda <fernanda.g.araujo@gmail.com>
2026-07-03 21:16:00 -0300

    commit de Modelo de leitura detalhado para Order

---
+++ src/main/java/com/algaworks/algashop/ordering/application/order/query/OrderItemDetailOutput.java
@@ -0,0 +1,24 @@
+package com.algaworks.algashop.ordering.application.order.query;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderItemDetailOutput {
+
+     private String id;
+     private String orderId;
+     private UUID productId;
+     private String productName;
+     private BigDecimal price;
+     private Integer quantity;
+     private BigDecimal totalAmount;
+}
